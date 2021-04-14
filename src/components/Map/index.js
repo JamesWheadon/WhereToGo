a94216b8fe32ba59cacb9b114cb546fae4378f5c
James Wheadon <james.wheadon@yahoo.com>
2021-04-14 17:38:05 +0100

    add .toml for netlify

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -34,6 +34,7 @@ const Map = () => {
 
   return (
     <>
+      <h3>Click where you would like to go!</h3>
       <canvas id="map" height="1080px" width="900px" onClick={getCursorPosition} ref={canvasRef}></canvas>
     </>
   );
