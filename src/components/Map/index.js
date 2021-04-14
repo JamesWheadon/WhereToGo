9011f95ec709a6f02350a34e3cf5e3f51091ff30
James Wheadon <james.wheadon@yahoo.com>
2021-04-14 17:14:19 +0100

    event listener on map

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -2,21 +2,24 @@ import React from 'react';
 
 const Map = () => {
 
-  const getCursorPosition = (canvas, event) => {
+  const locaitons = [{id: 1, x: 177, y: 800}, {id: 2, x: 229, y: 385}, {id: 3, x: 180, y: 531}, {id: 4, x: 443, y: 726}]
+
+  const getCursorPosition = (event) => {
+    const x = event.clientX
+    const y = event.clientY
+    console.log("x: " + x + " y: " + y)
+  }
+
+  /*const getCursorPosition = (canvas, event) => {
     const rect = canvas.getBoundingClientRect()
     const x = event.clientX - rect.left
     const y = event.clientY - rect.top
     console.log("x: " + x + " y: " + y)
-}
-
-  const canvas = document.querySelector('canvas')
-  canvas.addEventListener('mousedown', function(e) {
-      getCursorPosition(canvas, e)
-  })
+  }*/
 
   return (
     <>
-      <canvas id="map" height="1080px" width="900px"></canvas>
+      <canvas id="map" height="1080px" width="900px" onClick={getCursorPosition}></canvas>
     </>
   );
 };
