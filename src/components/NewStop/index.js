f1f809198f6abffc3ad03426dbe6dac42390920a
James Wheadon <james.wheadon@yahoo.com>
2021-04-15 17:40:05 +0100

    button to add new stop to road trip

---
+++ src/components/NewStop/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NewStop = ({clickFunction}) => {
+    return <button id="newStop" onClick={clickFunction}>Add another stop</button>
+}
+
+export default NewStop;
--- src/pages/RoadTrip/index.js
+++ src/pages/RoadTrip/index.js
@@ -1,28 +1,31 @@
 import React, { useState, useEffect } from 'react';
-import { Map, Location } from '../../components';
+import { Map, Location, NewStop } from '../../components';
 import { Header } from '../../layout';
 
 const RoadTrip = ({ destinations }) => {
     const [stops, setStops] = useState([])
     const [locations, setLocations] = useState([])
+    const [numStops, setNumStops] = useState(1)
 
     useEffect(() => {
         getStops();
-    }, [])
+    }, [numStops])
 
     const getStops = () => {
         let copy = destinations.map(m => m);
         let placesSummary = [];
         let places = [];
-        while (places.length < 3) {
+        while (places.length < numStops) {
             const random = Math.floor(Math.random() * copy.length);
             const stop = copy.splice(random, 1)[0];
-            placesSummary.push(stop)
-            places.push(stop.id)
+            placesSummary.push(stop);
+            places.push(stop.id);
         }
-        setLocations(placesSummary)
-        setStops(places)
-    }
+        setLocations(placesSummary);
+        setStops(places);
+    };
+
+    const newStop = () => setNumStops(prevState => prevState + 1);
 
     const renderStops = () => {
       return locations.map(l =>
@@ -35,6 +38,7 @@ const RoadTrip = ({ destinations }) => {
             <Header />
             <h2>Ready To Go?</h2>
             <Map interactive={false} stops={stops} />
+            <NewStop clickFunction={newStop}/>
             {renderStops()}
         </main>
     )
