8cb889eb5cd7d1c61e8bda86e5ed9352c6ef2c1e
James Wheadon <james.wheadon@yahoo.com>
2021-04-19 16:37:29 +0100

    test interactive map click, mock window.location.assign

--- src/components/Map/test.js
+++ src/components/Map/test.js
@@ -3,13 +3,28 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 describe('Interactive Map', () => {
+
+    delete window.location
+
+    window.location = {
+      assign: jest.fn(),
+    }
+
     beforeEach(() => {
         const testStops = []
         render(<Map interactive={true} stops={testStops}/>);
     });
 
-    test("renders the canvas", () => {
+    test("renders the canvas with correct dimensions", () => {
         const canvas = screen.getByRole('img', { id: 'map' });
         expect(canvas).toBeInTheDocument();
+        expect(canvas.width).toEqual(900);
+        expect(canvas.height).toEqual(1080);
+    });
+
+    test("redirects page to closest destination on click", () => {
+        const canvas = screen.getByRole('img', { id: 'map' });
+        userEvent.click(canvas);
+        expect(window.location.assign).toBeCalled();
     });
 });
